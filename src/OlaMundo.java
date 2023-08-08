967be128a3904661a79b94bf304dc851d910ff2f
João Miguel <115197583+joaomiguelg@users.noreply.github.com>
2023-08-08 12:21:26 -0300

    first

---
+++ src/OlaMundo.java
@@ -0,0 +1,12 @@
+
+public class OlaMundo {
+
+    
+    public static void main(String[] args) {
+        String meuPrimeiroPrograma;
+        meuPrimeiroPrograma = "Hello World";
+
+        System.out.print(meuPrimeiroPrograma);
+    }
+
+}
